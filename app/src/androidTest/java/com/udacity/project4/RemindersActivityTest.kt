817ce5b56a9c1d909532cb2755c18b50644f3bfd
Franek <poczta.f@protonmail.com>
2021-03-22 20:08:57 +0100

    Add end-to-end test

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,28 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.authentication.AuthenticationViewModel
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -28,6 +40,9 @@ class RemindersActivityTest :
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
 
+    // An idling resource that waits for Data Binding to have no pending bindings.
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
      * at this step we will initialize Koin related code to be able to use it in out testing.
@@ -42,6 +57,9 @@ class RemindersActivityTest :
                     appContext,
                     get() as ReminderDataSource
                 )
+            }
+            viewModel {
+                AuthenticationViewModel(appContext)
             }
             single {
                 SaveReminderViewModel(
@@ -65,7 +83,44 @@ class RemindersActivityTest :
         }
     }
 
+    /**
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
 
-//    TODO: add End to End testing to the app
+    /**
+     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Test
+    fun addReminder() = runBlocking {
+        // Start up Reminders screen.
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Click on the add reminder FAB, add all data and save
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(typeText("title"))
+        onView(withId(R.id.reminderDescription)).perform(typeText("description"))
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.save_button)).perform(click())
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // Verify reminder is displayed on screen in the reminders list.
+        onView(withText("title")).check(matches(isDisplayed()))
+        onView(withText("description")).check(matches(isDisplayed()))
+        onView(withText("My Location")).check(matches(isDisplayed()))
+
+        // Make sure the activity is closed before resetting the db:
+        activityScenario.close()
+    }
 
 }
