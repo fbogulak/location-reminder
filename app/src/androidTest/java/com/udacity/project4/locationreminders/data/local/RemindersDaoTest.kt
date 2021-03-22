747fffd14c4bcb79276c6d6e9c52d94ffc945040
Franek <poczta.f@protonmail.com>
2021-03-22 18:21:26 +0100

    Add test for RemindersDao

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+import com.google.android.gms.tasks.Task
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 
 import org.junit.Before;
@@ -25,6 +26,59 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun initDb() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+    
+    @Test
+    fun insertReminderAndGetById() = runBlockingTest {
+        // GIVEN - Insert a reminder.
+        val reminder = ReminderDTO("title", "description", "location", 1.0, 1.1)
+        database.reminderDao().saveReminder(reminder)
+
+        // WHEN - Get the reminder by id from the database.
+        val loaded = database.reminderDao().getReminderById(reminder.id)
+
+        // THEN - The loaded data contains the expected values.
+        assertThat(loaded as ReminderDTO, notNullValue())
+        assertThat(loaded.id, `is`(reminder.id))
+        assertThat(loaded.title, `is`(reminder.title))
+        assertThat(loaded.description, `is`(reminder.description))
+        assertThat(loaded.location, `is`(reminder.location))
+        assertThat(loaded.latitude, `is`(reminder.latitude))
+        assertThat(loaded.longitude, `is`(reminder.longitude))
+    }
+    
+    @Test
+    fun insertRemindersAndGetAllReminders() = runBlockingTest {
+        // Given - insert reminders
+        val reminders = listOf(
+            ReminderDTO("title1", "description1", "location1", 1.0, 1.1),
+            ReminderDTO("title2", "description2", "location2", 2.0, 2.2),
+            ReminderDTO("title3", "description3", "location3", 3.0, 3.3)
+        )
+        for (reminder in reminders)
+            database.reminderDao().saveReminder(reminder)
+
+        // When - get reminders from database
+        val loaded = database.reminderDao().getReminders()
+
+        // Then - loaded reminders are equals to the original
+        assertThat(loaded, `is`(reminders))
+    }
 }
